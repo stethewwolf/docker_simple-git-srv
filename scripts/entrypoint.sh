8c45171bd5114a6bc5dc13d88ef5bd60505a6b4d
Stefano Prina <stethewwolf@posteo.net>
2023-11-27 12:53:52 +0100

    updated entrypoint

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -6,9 +6,11 @@ source /etc/environment
 # print environment
 /usr/local/bin/print-gitolite-env.sh
 
-# mv gitolite config file
-mv /etc/gitweb.conf $GIT_HOME_DIR/gitweb.conf
-ln -s $GIT_HOME_DIR/gitweb.conf /etc/gitweb.conf
+# mv gitolite config file, if not yet done
+if [ ! -L "/etc/gitweb.conf" ]; then
+mv  -v /etc/gitweb.conf $GIT_HOME_DIR/gitweb.conf
+ln -sv $GIT_HOME_DIR/gitweb.conf /etc/gitweb.conf
+fi
 
 # make sure permission are correct
 chown -R $GIT_USER: $GIT_HOME_DIR
@@ -17,6 +19,12 @@ chown -R $GIT_USER: $GIT_HOME_DIR
 # this script is executed only if the file $GIT_HOME_DIR/.gitolite.rc does not exists
 if [ ! -f $GIT_HOME_DIR/.gitolite.rc ]; then
     su -c '/usr/local/bin/setup-gitolite.sh' $GIT_USER
+fi
+
+# setup gitserver management repo
+# this script create the repo server-admin and adds relevant config files
+if [ ! -d "$GIT_HOME_DIR/repositories/server-admin.git" ]; then
+    su -c '/usr/local/bin/create-server-admin-repo.sh' $GIT_USER
 fi
 
 # start sshd
