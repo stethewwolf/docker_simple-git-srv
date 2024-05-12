b468b7fe4271b033fab30bd4c100098b4e7164a8
Stefano Prina <stethewwolf@posteo.net>
2024-05-12 15:58:22 +0200

    re-implemented git mirroring

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -17,9 +17,7 @@ chown -vR $GIT_USER: $GIT_HOME_DIR
 
 # setup gitolite
 # this script is executed only if the file $GIT_HOME_DIR/.gitolite.rc does not exists
-if [ ! -f $GIT_HOME_DIR/.gitolite.rc ]; then
-    su -c '/usr/local/bin/setup-gitolite.sh' $GIT_USER
-fi
+su -c '/usr/local/bin/setup-gitolite.sh' $GIT_USER
 
 # setup gitserver management repo
 # this script create the repo server-admin and adds relevant config files
