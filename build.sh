4ac2654581ae6afa4b3271f6e7919e1a7aa7d0ef
Stefano Prina <stethewwolf@posteo.net>
2024-05-28 16:10:54 +0200

    added build.sh

---
+++ build.sh
@@ -0,0 +1,15 @@
+#/bin/bash
+
+arch=$(dpkg --print-architecture)
+
+IMAGE_TAG="latest"
+IMAGE_NAME="simple-git-server"
+REGISTRY_URL="fenrir.stobi.local:5000"
+
+if [[ $arch == "arm64" ]]; then
+  IMAGE_TAG="latest-arm64"
+fi
+
+docker build -t $REGISTRY_URL/$IMAGE_NAME:$IMAGE_TAG .
+docker push $REGISTRY_URL/$IMAGE_NAME:$IMAGE_TAG
+
